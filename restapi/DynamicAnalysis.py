580b063bc696f3e0691485eb099a9b26bb38953d
Eric P <eric.paquet@nuance.com>
2020-01-28 02:24:51 -0500

    Minor tweaks

--- restapi/DynamicAnalysis.py
+++ restapi/DynamicAnalysis.py
@@ -5,6 +5,12 @@ from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
 app_name="DynamicAnalysis"
 data_dir=os.environ.get("HOME")+ "/data/"
 
+#Uncomment only when debugging through Burp
+#import urllib3
+#urllib3.disable_warnings()
+#verifyCert=False
+verifyCert=True
+
 class DynamicAnalysis:
     headers = {"User-Agent": "Veracode DynamicAnalysis REST API Client"}
 
@@ -12,7 +18,7 @@ class DynamicAnalysis:
         try:
             scan_details_url = data["_embedded"]["analyses"][0]["_links"]["scans"]["href"]
             log.debug("Sending request to %s", scan_details_url)
-            response = requests.get(scan_details_url, auth=RequestsAuthPluginVeracodeHMAC(), headers=self.headers)
+            response = requests.get(scan_details_url, auth=RequestsAuthPluginVeracodeHMAC(), headers=self.headers, verify=verifyCert)
 
         except requests.RequestException as e:
             log.error("Request for scan details failed.")
@@ -33,7 +39,7 @@ class DynamicAnalysis:
         try:
             arg1 = "name=" + scan_name
             log.debug("Sending request to %s", api_base)
-            response = requests.get(api_base+"?"+arg1, auth=RequestsAuthPluginVeracodeHMAC(), headers=self.headers)
+            response = requests.get(api_base+"?"+arg1, auth=RequestsAuthPluginVeracodeHMAC(), headers=self.headers, verify=verifyCert)
             data = response.json()
             if response.ok:
                 json_file_base = data_dir + "ScanSpec"
@@ -59,7 +65,6 @@ class DynamicAnalysis:
         with open(csv_filename) as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                auth_configuration = {}
                 scan = {}
                 scan["scan_config_request"] = {}
                 scan["scan_config_request"]["target_url"] = {}
@@ -81,6 +86,7 @@ class DynamicAnalysis:
 
                 # Add Crawl script
                 # TODO: Support crawl_script_file (or validate what's already here)
+                '''
                 if (row['crawl_script_file'] != ""):
                     crawl_script_path = row['base_path'] + "/" + row['crawl_script_file']
                     log.debug("Reading crawl script data from %s", crawl_script_path)
@@ -91,10 +97,12 @@ class DynamicAnalysis:
                             {   "crawl_script_data": { \
                                     "script_type": "SELENIUM", \
                                     "script_body": crawl_script_data \
-                                }\
+                                }, \
+                                "name": row['crawl_script_file'] \
                             }], \
                             "disabled": False \
                         }
+                '''
 
 
                 # Add Allowed Hosts
@@ -134,7 +142,6 @@ class DynamicAnalysis:
                                 "authtype":"FORM", \
                                 "login_script_data":{ \
                                     "script_body":login_script_data,\
-                                    #"script_name":row['login_script_file'],\
                                     "script_type": "SELENIUM" \
                                 } \
                             } \
@@ -149,7 +156,6 @@ class DynamicAnalysis:
                         scan["scan_config_request"]["auth_configuration"]["authentications"]["FORM"]["logout_script_data"] = \
                                     { \
                                         "script_body": logout_script_data, \
-                                        #"script_name": row['logout_script_file'], \
                                         "script_type": "SELENIUM" \
                                     }
 
@@ -164,7 +170,6 @@ class DynamicAnalysis:
                         {   "scripts": [ \
                             {   "crawl_script_data":{ \
                                     "script_body": crawl_script_data, \
-                                    #"script_name": row['crawl_script_file'], \
                                     "script_type": "SELENIUM" \
                                 }\
                             }], \
@@ -185,12 +190,12 @@ class DynamicAnalysis:
             url= api_base
             log.info("Sending POST request to %s", url)
             log.debug("POST Body: " + json.dumps(scan_request_data, sort_keys=True, indent=4))
-            response = requests.post(url, auth=RequestsAuthPluginVeracodeHMAC(), headers=self.headers, json=scan_request_data)
+            response = requests.post(url, auth=RequestsAuthPluginVeracodeHMAC(), headers=self.headers, json=scan_request_data, verify=verifyCert)
             if response.ok:
                 log.debug("Successful response: %s", str(response))
                 return response
             else:
-                log.warning("Request to create scan failed with %s code: %s", response.status_code, str(response))
+                log.warning("Request to create scan failed with %s code: %s", response.status_code, response.text)
                 sys.exit(1)
 
         except requests.RequestException as e:
@@ -199,7 +204,6 @@ class DynamicAnalysis:
             sys.exit(2)
 
 def check_args(args):
-    log.debug("Args: %s", str(args))
     log.debug("name = %s", args.name)
     log.debug("start_date = %s", args.start_date)
     log.debug("teamId = %s", args.teamId)
@@ -211,7 +215,7 @@ if __name__ == "__main__":
     start_date="2020-03-03T02:00+00:00"
     teamId = "101736"
     teamId = "132892"
-    csv_filename = "../sample_data/dynscan-dvna.csv"
+    csv_filename = "../data/dynscan-dvna.csv"
     scan_action = "create"
 
     ### Configure logging
@@ -226,7 +230,7 @@ if __name__ == "__main__":
     parser.add_argument('--action', help='Scan Action := [create, export]', dest="scan_action", default=scan_action)
     parser.add_argument('--name', help='Scan Name.', dest="name", default=name)
     parser.add_argument('--start', help='Start Date for scan.', dest="start_date", default=start_date)
-    parser.add_argument('--team', help='Team ID. Default: none.', dest="teamId", default=teamId)
+    parser.add_argument('--team', help='Team ID.', dest="teamId", default=teamId)
     parser.add_argument('--csvfile', help='CSV file name.', dest="csv_filename", default=csv_filename)
     args = parser.parse_args()
     log.debug("Args: %s", str(args))
@@ -240,13 +244,13 @@ if __name__ == "__main__":
         ### Build-up the scan request data
         # Set the visibility
         visibility = { "setup_type": "SEC_LEADS_ONLY" }
-        if (teamId != ""):
-            visibility ={ "setup_type" : "SEC_LEADS_AND_TEAM", "team_identifiers" : [ teamId ] }
+        if (args.teamId != ""):
+            visibility ={ "setup_type" : "SEC_LEADS_AND_TEAM", "team_identifiers" : [ args.teamId ] }
 
         # Set the schedule
         schedule = {}
         if (start_date != ""):
-            schedule = { "start_date": start_date, "duration": { "length": 1, "unit": "DAY" } }
+            schedule = { "start_date": args.start_date, "duration": { "length": 1, "unit": "DAY" } }
 
         # Get URLs and login scripts
         scans = a.get_scan_spec_from_csv(args.csv_filename)
